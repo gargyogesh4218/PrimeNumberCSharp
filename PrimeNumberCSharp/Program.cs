2336728977754616523e2edc28ffd0bc6b8ed592
yogesh kumar garg <gargyogesh105@gmail.com>
2023-05-04 00:33:40 +0530

    Initial commit

---
+++ PrimeNumberCSharp/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        Console.WriteLine("enter a number which to be check if it is prime or not");
+        int n= Convert.ToInt32(Console.ReadLine());
+        int count = 0;
+        for (int i=1; i<=n/2;i++)
+        {
+            if (n % i == 0)
+                count++;
+        }
+
+        if (count > 1)
+            Console.WriteLine("not prime");
+        else
+            Console.WriteLine(" prime");
+
+        Console.ReadLine();
+    }
+
+    
+}
